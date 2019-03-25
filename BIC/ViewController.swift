fb336a8e42c5cd6ad78f66511811bb3029f3a6cc
Iwan Siauw <evansiauw@gmail.com>
2019-03-25 16:35:47 -0400

    Initial Commit

---
+++ BIC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BIC
+//
+//  Created by Iwan Siauw on 3/25/19.
+//  Copyright © 2019 Iwan Siauw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
