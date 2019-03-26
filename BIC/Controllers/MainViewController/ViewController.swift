d875df89945869cb5b7b8295ade22071ac645fb7
Iwan Siauw <evansiauw@gmail.com>
2019-03-25 21:11:26 -0400

    Setup main view controller and navigation bar items

--- BIC/Controllers/MainViewController/ViewController.swift
+++ BIC/Controllers/MainViewController/ViewController.swift
@@ -9,10 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Scroll View
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        let screenSize = UIScreen.main.bounds
+        scrollView.contentSize = CGSize(width: screenSize.width, height: 2000)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    // Random button
+    let randomButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Test", for: .normal)
+        button.backgroundColor = .blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationBar()
+        mainScreen()
     }
+   
+    // Navigation bar setup on the main screen
+    func navigationBar(){
+        
+        // Navigation Item
+        navigationItem.title = "Bethel International Church"
+        
+        // Left Bar Menu Button
+        let hamburgerImage = UIImage(named: "hamburgerMenu")
+        let sideMenuButton = UIBarButtonItem(image: hamburgerImage, style: .plain, target: self, action: nil)
+        navigationItem.leftBarButtonItem = sideMenuButton
+        navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        // Right Bar Menu Button
+        let profileImage = UIImage(named: "profileMenu")
+        let profileMenuButton = UIBarButtonItem(image: profileImage, style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItem = profileMenuButton
+        navigationItem.rightBarButtonItem?.tintColor = .black
+    }
+    
+    // Main Screen setup
+    func mainScreen(){
+        
+        // Scroll View
+        self.view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+        
+        scrollView.addSubview(randomButton)
+        
+    }
+    
 
 
 }
